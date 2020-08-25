85b684074ca2f236cf015cad063798164fab4ae0
Jeff Michelot <jeffmichelot@gmail.com>
2020-08-25 19:35:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,14 @@
 def create_an_empty_array
+  empty_array = []
   
 end
 
 def create_an_array
-  
+   numbers = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
+  add_element_to_end_of_array.push(element)
   
 end
 
